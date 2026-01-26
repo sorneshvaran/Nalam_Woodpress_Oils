ef10b7825e56f512e852ea281c513be7c3e7cf6d
sorneshvaran <sorneshdr@gmail.com>
2026-01-26 17:02:34 +0530

    image retained

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -6,7 +6,7 @@ function Card(){
         {
             id: 1,
             name: "Groundnut Oil",
-            image: "./Groundnut_Oil_1L.png",
+            image: "src/Card/Groundnut_Oil_1L.png",
             quantity: "½L",
             price: "₹125",
             category: "cooking"
